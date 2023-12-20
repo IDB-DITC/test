516b36a190cebb6122a12724676aa918973968d9
DITC IDB <146737240+IDB-DITC@users.noreply.github.com>
2023-12-20 19:00:38 +0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ExampleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+		IRepository repo;
+		//public HomeController()
+		//{
+		//	repo = Repository.Current;
+		//}
+
+
+		public HomeController(IRepository _repo)
+		{
+			repo = _repo;
+		}
+
+
+
+		public ActionResult Index()
+		{
+			return View(repo.Products);
+		}
+	}
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,71 @@
+﻿using ExampleApp.Models;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExampleApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+
+		IRepository repo;
+		
+		//public ProductsController()
+		//{
+		//	repo = Repository.Current;
+		//}
+
+
+		public ProductsController(IRepository _repo)
+		{
+			repo = _repo;
+		}
+
+		public IHttpActionResult GetAll()
+		{
+			return Json(repo.Products);
+		}
+
+		//public IEnumerable<Product> GetAll()
+		//{
+		//	return repo.Products;
+		//}
+		public Product GetById(int id)
+		{
+			return repo.GetProduct(id);
+		}
+		[HttpGet]
+		[Route("api/products/noop")]
+		public IHttpActionResult NoOp()
+		{
+			return Ok("test ok");
+		}
+
+
+
+
+		public Product PostProduct(Product product)
+		{
+			return repo.SaveProduct(product);
+		}
+
+		
+
+		public Product PutProduct(Product product)
+		{
+			return repo.UpdateProduct(product);
+		}
+
+		public IHttpActionResult DeleteProduct(int id)
+		{
+			 repo.DeleteProduct(id);
+
+			return ResponseMessage(new HttpResponseMessage(HttpStatusCode.Accepted));
+
+			//return StatusCode(System.Net.HttpStatusCode.Accepted);
+			//return Ok();
+
+		}
+	}
+}
